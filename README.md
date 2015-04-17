edd954cf1a21b2c6ffe923613c6d249d069bba6d
Bas Beelen <bjgbeelen@gmail.com>
2015-04-17 09:32:59 +0200

    Added more dependencies for example app

--- README.md
+++ README.md
@@ -3,9 +3,11 @@ Xebia Innovation Day #2 - Akka Persistence 2.4-SNAPSHOT with Cassandra Journal
 
 Goal
 ----
-Getting Akka Persistence 2.4 to work with a Cassandra Journal. This Journal plugin is going to be largely based on the Akka Persistence Cassandra plugin which can be found at [here](https://github.com/krasserm/akka-persistence-cassandra/)
+**#1** Getting Akka Persistence 2.4 to work with a Cassandra Journal. This Journal plugin is going to be largely based on the Akka Persistence Cassandra plugin which can be found at [here](https://github.com/krasserm/akka-persistence-cassandra/)
 
 
 Related information and documentation
 -------------------------------------
 [Journal Plugin API](http://doc.akka.io/docs/akka/2.3.9/scala/persistence.html#journal-plugin-api)
+
+
--- src/main/scala/akka/persistence/cassandra/journal/CassandraJournal.scala
+++ src/main/scala/akka/persistence/cassandra/journal/CassandraJournal.scala
@@ -38,30 +38,32 @@ class CassandraJournal extends AsyncWriteJournal with CassandraRecovery with Cas
   def asyncWriteMessages(messages: Seq[PersistentRepr]): Future[Unit] = executeBatch { batch =>
     messages.foreach { m =>
       val pnr = partitionNr(m.sequenceNr)
-      if (partitionNew(m.sequenceNr)) batch.add(preparedWriteHeader.bind(m.processorId, pnr: JLong))
-      batch.add(preparedWriteMessage.bind(m.processorId, pnr: JLong, m.sequenceNr: JLong, persistentToByteBuffer(m)))
+      if (partitionNew(m.sequenceNr)) batch.add(preparedWriteHeader.bind(m.persistenceId, pnr: JLong))
+      batch.add(preparedWriteMessage.bind(m.persistenceId, pnr: JLong, m.sequenceNr: JLong, persistentToByteBuffer(m)))
     }
   }
 
-  def asyncWriteConfirmations(confirmations: Seq[PersistentConfirmation]): Future[Unit] = executeBatch { batch =>
-    confirmations.foreach { c =>
-      batch.add((preparedConfirmMessage.bind(c.persistenceId, partitionNr(c.sequenceNr): JLong, c.sequenceNr: JLong, confirmMarker(c.channelId))))
-    }
-  }
+  // Removed in Akka Persistence 2.4-SNAPSHOT
+  // def asyncWriteConfirmations(confirmations: Seq[PersistentConfirmation]): Future[Unit] = executeBatch { batch =>
+  //   confirmations.foreach { c =>
+  //     batch.add((preparedConfirmMessage.bind(c.persistenceId, partitionNr(c.sequenceNr): JLong, c.sequenceNr: JLong, confirmMarker(c.channelId))))
+  //   }
+  // }
 
-  def asyncDeleteMessages(messageIds: Seq[PersistentId], permanent: Boolean): Future[Unit] = executeBatch { batch =>
-    messageIds.foreach { mid =>
-      val stmt =
-        if (permanent) preparedDeletePermanent.bind(mid.processorId, partitionNr(mid.sequenceNr): JLong, mid.sequenceNr: JLong)
-        else preparedDeleteLogical.bind(mid.processorId, partitionNr(mid.sequenceNr): JLong, mid.sequenceNr: JLong)
-      batch.add(stmt)
-    }
-  }
+  // Removed in Akka Persistence 2.4-SNAPSHOT
+  // def asyncDeleteMessages(messageIds: Seq[PersistentId], permanent: Boolean): Future[Unit] = executeBatch { batch =>
+  //   messageIds.foreach { mid =>
+  //     val stmt =
+  //       if (permanent) preparedDeletePermanent.bind(mid.persistenceId, partitionNr(mid.sequenceNr): JLong, mid.sequenceNr: JLong)
+  //       else preparedDeleteLogical.bind(mid.persistenceId, partitionNr(mid.sequenceNr): JLong, mid.sequenceNr: JLong)
+  //     batch.add(stmt)
+  //   }
+  // }
 
-  def asyncDeleteMessagesTo(processorId: String, toSequenceNr: Long, permanent: Boolean): Future[Unit] = {
-    val fromSequenceNr = readLowestSequenceNr(processorId, 1L)
+  def asyncDeleteMessagesTo(persistenceId: String, toSequenceNr: Long, permanent: Boolean): Future[Unit] = {
+    val fromSequenceNr = readLowestSequenceNr(persistenceId, 1L)
     val asyncDeletions = (fromSequenceNr to toSequenceNr).grouped(persistence.settings.journal.maxDeletionBatchSize).map { group =>
-      asyncDeleteMessages(group map (PersistentIdImpl(processorId, _)), permanent)
+      asyncDeleteMessages(group map (PersistentIdImpl(persistenceId, _)), permanent)
     }
     Future.sequence(asyncDeletions).map(_ => ())
   }
